2e867d3254c9f8398bef9d235f67b781cd73521b
twu29 <twu29@uw.edu>
2026-02-28 02:52:36 -0800

    tweak the questions + use real data to draw 2 graphs

--- js/sketches/sketch_manager.js
+++ js/sketches/sketch_manager.js
@@ -9,8 +9,8 @@ function startP5() {
     // --- Sketch manager ----------------------------------------------------
     function SketchManager() {
         // core layout settings (canvas size only)
-        this.width = 600; // content width
-        this.height = 520; // content height
+        this.width = 680; // content width
+        this.height = 620; // content height
         this.margin = { top: 0, left: 80, bottom: 40, right: 10 };
         this.canvasWidth = this.width + this.margin.left + this.margin.right;
         this.canvasHeight = this.height + this.margin.top + this.margin.bottom;
